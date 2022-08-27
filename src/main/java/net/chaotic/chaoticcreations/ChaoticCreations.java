9da47f753c687830924237eb7660af4cf839f5e6
RatherChaotic <103642373+JustACodingLad@users.noreply.github.com>
2022-08-26 22:31:26 -0400

    Some simple housekeeping work

---
+++ src/main/java/net/chaotic/chaoticcreations/ChaoticCreations.java
@@ -0,0 +1,14 @@
+package net.chaotic.chaoticcreations;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ChaoticCreations implements ModInitializer {
+	public static final String MOD_ID = "chaoticcreations";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/chaotic/chaoticcreations/ChaoticCreationsClient.java
@@ -0,0 +1,11 @@
+package net.chaotic.chaoticcreations;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChaoticCreationsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
